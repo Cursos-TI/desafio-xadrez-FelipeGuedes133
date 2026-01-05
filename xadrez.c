4ebf31739d1e5fc2dbffab811a52f82a15430ab2
Felipe Guedes <felipeguedes.oficial133@gmail.com>
2026-01-05 18:09:09 +0000

    Desafio Xadrex criado em C + movimentação de três peças

--- xadrez.c
+++ xadrez.c
@@ -5,8 +5,52 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+
+    // ================= TORRE =================
+    // Bem, a torre sempre se move em uma linha reta.
+    // Como solicitado no desafio, ela deve se mover 5 casas para a direita.
+    // Usando a estrutura: FOR
+
+    int casasTorre = 5;
+
+    printf("Movimento da TORRE:\n");
+
+    for (int i = 1; i <= casasTorre; i++) {
+        printf("Direita\n");
+    }
+
+    // ================= BISPO =================
+    // O Bispo sempre se move na diagonal no tabuleiro.
+    // como solicitado no desafio, ele deve se mover 5 casas na diagonal para cima e à direita.
+    // Usando a estrutura: WHILE
+
+    int casasBispo = 5;
+    int contadorBispo = 1;
+
+    printf("\nMovimento do BISPO:\n");
+
+    while (contadorBispo <= casasBispo) {
+        printf("Cima, Direita\n");
+        contadorBispo++;
+    }
+
+    // ================= RAINHA =================
+    // já a rainha pode se mover em todas as direções no tabuleiro.
+    // como solicitado no desafio, a rainha deve se movimentar 8 casas para a esquerda.
+    // usando a estrutura: DO-WHILE
+
+    int casasRainha = 8;
+    int contadorRainha = 1;
+
+    printf("\nMovimento da RAINHA:\n");
+
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= casasRainha);
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
